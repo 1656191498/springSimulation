3420a44c8c5898a4409d78d74de8dcc8590f1763
xiaweizhe <xiaweizhe@doraemail.com>
2020-09-22 18:31:23 +0800

    设置二级缓存

--- src/main/java/com/azhe/mySpring/applicationContext/SpringApplicationContext.java
+++ src/main/java/com/azhe/mySpring/applicationContext/SpringApplicationContext.java
@@ -31,6 +31,11 @@ public class SpringApplicationContext {
      * 单例池
      */
     private final Map<String, Object> singletonObjects = new ConcurrentHashMap<>(256);
+
+    /**
+     * 暴露对象，二级缓存
+     */
+    public static Map<String,Object> earlySingletonObjects = new ConcurrentHashMap<>(256);
     /**
      * bean名称列表
      */
@@ -39,6 +44,10 @@ public class SpringApplicationContext {
      * 过滤重复扫描的包
      */
     public static Set<String> packageNameSet = new HashSet<>();
+    /**
+     * 是否正在创建
+     */
+    public static Set<String> isCreateBean = new HashSet<>();
     /**
      *
      * @param componentClasses 将配置类注册入容器中
@@ -85,29 +94,102 @@ public class SpringApplicationContext {
      */
     private <T> T getBean(String beanName,Class<T> requiredType){
         //如果单例池中没有则创建一个
+        Object singletonObject = doGetBean(beanName);
+        return (T) singletonObject;
+    }
+
+    /**
+     * 真正创建bean
+     * @param beanName
+     * @return
+     */
+    private Object doGetBean(String beanName){
         Object singletonObject = singletonObjects.get(beanName);
         if(singletonObject == null){
+            //如果正在创建的话
+            if(isCreateBean.contains(beanName)){
+                //从二级缓存中拿
+                return earlySingletonObjects.get(beanName);
+            }else {
+                isCreateBean.add(beanName);
+            }
             try {
                 BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
                 singletonObject = beanDefinition.getBeanClass().newInstance();
+                earlySingletonObjects.put(beanName,singletonObject);
                 //给属性赋值
                 Field[] declaredFields = singletonObject.getClass().getDeclaredFields();
-                //遍历所有字段看是否有Autowired
-                for (Field field:declaredFields){
-                    //还要解决循环依赖
-                    if(field.isAnnotationPresent(Autowired.class)){
-                        field.setAccessible(true);
-                        String beanNameN = field.getType().getSimpleName().replaceFirst(field.getType().getSimpleName().substring(0, 1), field.getType().getSimpleName().substring(0, 1).toLowerCase());
-                        field.set(singletonObject,getBean(beanNameN,field.getType()));
-                    }
-                }
+                populateBean(singletonObject,declaredFields);
                 singletonObjects.put(beanName,singletonObject);
+                //将该bean从缓存池中移除
+                earlySingletonObjects.remove(beanName);
+                isCreateBean.remove(beanName);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+        return singletonObject;
+    }
 
-        return (T) singletonObject;
+    /**
+     * 属性赋值
+     * @param singletonObject
+     * @param declaredFields
+     */
+    private void populateBean(Object singletonObject, Field[] declaredFields) {
+        try {
+            //遍历所有字段看是否有Autowired,按照类型注入
+            for (Field field:declaredFields){
+                //还要解决循环依赖
+                if(field.isAnnotationPresent(Autowired.class)){
+                    field.setAccessible(true);
+//                    String beanNameN = generateBeanName(field.getType());
+                    //按照类型匹配
+                    List<String> populateBean = getPopulateBean(field.getType());
+                    String beanNameN = field.getName();
+                    if(populateBean.size()==0){
+                        throw new RuntimeException("不能够自动注入该类型:"+beanNameN);
+                    }
+                    String beanName = beanNameN;
+                    //多个实现取属性名相等的
+                    if(populateBean.size()>1){
+                        beanName = populateBean.stream().filter(bean -> bean.equals(beanNameN)).findFirst().orElse(null);
+                    }
+                    if(beanName!=null){
+                        field.set(singletonObject,doGetBean(beanName));
+                    }else {
+                        throw new RuntimeException("容器中不存在该类型:"+beanNameN);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 按照类型匹配字段,可能存在多个实现
+     * @param clazz
+     * @return
+     */
+    private List<String> getPopulateBean(Class clazz){
+        List<String> list = new ArrayList<>();
+        for (String beanName:beanDefinitionNames){
+            Object bean = doGetBean(beanName);
+            if(clazz.isAssignableFrom(bean.getClass())){
+                list.add(beanName);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 生成beanName
+     * @param clazz
+     * @return
+     */
+    public String generateBeanName(Class clazz){
+        return clazz.getSimpleName().replaceFirst(clazz.getSimpleName().substring(0, 1), clazz.getSimpleName().substring(0, 1).toLowerCase());
     }
 
     /**
